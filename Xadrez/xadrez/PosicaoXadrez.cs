91eaf8ddc8bb61a56a5105fd8f0ea95d95108eb6
Jean-Guedes <jean-guedes@outlook.com>
2021-11-04 14:50:38 -0300

    Criada a classe PosicaoXadrez

---
+++ Xadrez/xadrez/PosicaoXadrez.cs
@@ -0,0 +1,27 @@
+using tabuleiro;
+
+namespace Xadrez
+{
+    class PosicaoXadrez
+    {
+        public char coluna { get; set; }
+        public int linha { get; set; }
+
+        public PosicaoXadrez(char coluna, int linha)
+        {
+            this.linha = linha;
+            this.coluna = coluna;
+        }
+
+        public Posicao toPosicao()
+        {
+            return new Posicao(8-linha, coluna-'a');
+        }
+
+        public override string ToString()
+        {
+            // return ""coluna+linha;
+            return coluna+linha.ToString();
+        }
+    }
+}
